7e5827cc9bc5c9d941288d9db249302ddf59415f
Dreampop <sashareconquista@proton.me>
2024-09-24 23:22:03 +0200

    Guardando la pagina enu un repo

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("vivavox");
+
+button.addEventListener("click", () => {
+    window.location.replace("https://invidious.privacyredirect.com/watch?v=QxqiI50WPoM");
+})
